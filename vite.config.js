031f9573688dd3a625861039c0a4c74543aa0574
tghyhy23 <s4053650@rmit.edu.vn>
2025-10-01 23:48:33 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',            // với repo user-site <username>.github.io phải là "/"
   plugins: [react()],
+  build: { outDir: 'dist' } // mặc định của Vite, để nguyên
 })
